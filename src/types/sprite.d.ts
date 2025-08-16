fe1f09b1408bad42efb7c950cbd29418cc465d65
GarcesSebastian <sebastiangarces152@gmail.com>
2025-08-16 04:29:11 -0500

    fix: problems with linter

--- src/types/sprite.d.ts
+++ src/types/sprite.d.ts
@@ -1,10 +1,13 @@
 import { Vector } from '@/lib/common/Vector';
 
 interface SpriteProps {
-  position: typeof Vector;
   src: string;
-  spriteGrid: { rows: number; cols: number };
-  ignoreFrames: number[];
+  spriteGrid: {
+    rows: number;
+    cols: number;
+  };
+  position: Vector;
+  ignoreFrames?: number[];
   pattern?: string[];
-  dragging: boolean;
+  dragging?: boolean;
 }
